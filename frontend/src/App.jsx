53d146d4ee4b72c5311abf047d26b58b1f03ae37
kaled182 <163221067+kaled182@users.noreply.github.com>
2025-03-29 13:56:00 -0300

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,1 +1,15 @@
+import MapViewer from './MapViewer';
+import './App.css';
 
+function App() {
+  return (
+    <div className="app">
+      <header>
+        <h1>MapsProve</h1>
+      </header>
+      <MapViewer />
+    </div>
+  );
+}
+
+export default App;
